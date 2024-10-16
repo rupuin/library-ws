f8852f6991425b0882730132afd612e7aef29237
rupuin <ignas.staskevicius@vinted.com>
2024-10-16 22:30:34 +0300

    add modules loader for cleaner imports

---
+++ lib/loader.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'book/book'
+require_relative 'book/parser'
+require_relative 'book/repository'
+require_relative 'user/user'
+require_relative 'user/repository'
+require_relative 'database/database'
+require_relative 'library/service'
