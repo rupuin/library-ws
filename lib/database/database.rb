6c26465caf368dad80d4e744ea5b3e14db20e3a1
rupuin <ignas.staskevicius@vinted.com>
2024-10-16 22:28:42 +0300

    add database module

---
+++ lib/database/database.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module Database # rubocop:disable Style/Documentation
+  class << self
+    def connect(db_file)
+      @connect ||= setup(db_file)
+    end
+
+    def close
+      return unless @connect
+
+      @connect.close
+      @connect = nil
+    end
+
+    private
+
+    def setup(db_file)
+      db = SQLite3::Database.new(db_file)
+      db.results_as_hash = true
+      enable_foreign_keys(db)
+      create_user_table(db)
+      create_book_table(db)
+      db
+    end
+
+    def enable_foreign_keys(db)
+      db.execute('PRAGMA foreign_keys = ON')
+    end
+
+    def create_user_table(db)
+      db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(100),
+    password VARCHAR(60),
+    borrowed_book INTEGER,
+    FOREIGN KEY (borrowed_book) REFERENCES book(id)
+  );
+      SQL
+    end
+
+    def create_book_table(db)
+      db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS book (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(100),
+    author VARCHAR(100),
+    year INTEGER,
+    borrowed_by INTEGER,
+    FOREIGN KEY (borrowed_by) REFERENCES user(id)
+    );
+      SQL
+    end
+  end
+end
